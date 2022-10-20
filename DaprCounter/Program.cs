48409865241894d0f8579b79a17fa8c294a65ff3
ShuaiHua Du <shuaihua.du@outlook.com>
2022-10-20 23:41:10 +0800

    Init commit.

---
+++ DaprCounter/Program.cs
@@ -0,0 +1,16 @@
+﻿using Dapr.Client;
+
+const string storeName = "memory";
+const string key = "counter";
+
+var daprClient = new DaprClientBuilder().Build();
+
+var counter = await daprClient.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+    Console.WriteLine($"Counter = {counter++}");
+
+    await daprClient.SaveStateAsync(storeName, key, counter);
+    await Task.Delay(1000);
+}
---
+++ DaprMultiContainer/MyFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyFrontEnd.Models;
+
+namespace MyFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DaprClient _daprClient;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(HttpMethod.Get, "MyBackEnd", "WeatherForecast");
+            //ViewData.Add("WeatherForecast", forecasts);
+            ViewData["WeatherForecastData"] = forecasts;
+        }
+    }
+}
